27d7f7bfba4490ffbcf6d5f2d93724a7b67261e8
Wout Rombouts <woutrombouts18@gmail.com>
2023-02-26 23:40:31 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# Install generic libraries
 apt update
-apt install python3-pip
+apt install nano
+
+# Install CircuitPython
+apt install -y python3 git python3-pip
+update-alternatives --install /usr/bin/python python $(which python2) 1
+update-alternatives --install /usr/bin/python python $(which python3) 2
+apt update
+pip3 install Jetson.GPIO
+groupadd -f -r gpio
+usermod -a -G gpio mcqueen
+cp /home/mcqueen/.local/lib/python3.6/site-packages/Jetson/GPIO/99-gpio.rules /etc/udev/rules.d/
+udevadm control --reload-rules && udevadm trigger
+pip3 install adafruit-blinka
+
+# Install additional requirements
 pip3 install -r requirements.txt
 
+# Configure system service
 chmod +x mcqueen-service.sh
 cp mcqueen.service /etc/systemd/system/mcqueen.service
 systemctl daemon-reload
