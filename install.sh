b1046e6c9d6525a38ff8c2be459a8b02ac43d197
Wout Rombouts <woutrombouts18@gmail.com>
2023-02-26 22:59:41 +0100

    Updated install script and added controller testfile

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-chmod +x mcqueen-service.sh
 
+apt update
+apt install python3-pip
+pip3 install -r requirements.txt
+
+chmod +x mcqueen-service.sh
 cp mcqueen.service /etc/systemd/system/mcqueen.service
 systemctl daemon-reload
 systemctl enable mcqueen.service
