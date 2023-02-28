efb3061199fef8f94c966035fcf9da4b6565faad
Wout Rombouts <woutrombouts18@gmail.com>
2023-02-28 15:59:12 +0100

    imu debugging

--- main_program.py
+++ main_program.py
@@ -44,11 +44,11 @@ class McQueen:
         self.servo_pid = PID(1, 0.1, 0.05, setpoint=90)
         self.servo_pid.output_limits = (0, 180)
         self.servo_pid.error_map = self.transform_heading_to_angle
-        self.servo_pid.sample_time = 0.01
+        self.servo_pid.sample_time = 1
         # Max safe speed = 0.3,  Slow = 0.1,  AVG = 0.2
         self.motor_pid = PID(1, 0.1, 0.05, setpoint=0.5)
         self.motor_pid.output_limits = (0, 0.0)
-        self.motor_pid.sample_time = 0.01
+        self.motor_pid.sample_time = 1
 
         try:
             #self.test_servo()
@@ -69,7 +69,7 @@ class McQueen:
             self.calculate_heading()
             self.cycle_loop_motor()
             self.cycle_loop_steering()
-            time.sleep(0.1)
+            time.sleep(1)
 
     def safe_stop(self):
         print("Handling safe stop...")
