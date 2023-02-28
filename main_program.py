e65419f2e9903d2f82ff82842660f1e94485e165
Wout Rombouts <woutrombouts18@gmail.com>
2023-02-28 16:42:03 +0100

    testing

--- main_program.py
+++ main_program.py
@@ -46,11 +46,11 @@ class McQueen:
         print("Initialising controllers...")
         self.servo_pid = PID(1, 0, 0, setpoint=0)
         self.servo_pid.output_limits = (-90, 90)
-        self.servo_pid.sample_time = 0.1
+        self.servo_pid.sample_time = 0.5
         # Max safe speed = 0.3,  Slow = 0.1,  AVG = 0.2
         self.motor_pid = PID(1, 0.1, 0.05, setpoint=0.5)
         self.motor_pid.output_limits = (0, 0.0)
-        self.motor_pid.sample_time = 0.1
+        self.motor_pid.sample_time = 0.5
 
         try:
             # self.test_servo()
@@ -82,6 +82,7 @@ class McQueen:
             self.calculate_heading()
             self.cycle_loop_motor()
             self.cycle_loop_steering()
+            time.sleep(0.1)
 
     def safe_stop(self):
         self.actuator_motor.throttle = 0
