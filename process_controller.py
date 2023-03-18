2d9f2f541ebbab3905041d5f61f31e96f4ca5ab3
Wout Rombouts <woutrombouts18@gmail.com>
2023-03-18 22:37:36 +0100

    s

--- process_controller.py
+++ process_controller.py
@@ -9,40 +9,7 @@ def print_remove(joy):
 
 def key_received(key):
 
-    if key.keytype == "Axis" and key.number == 2:
-        # Right joystick, left - right
-        # Steering
-        print("Steering")
-        print(vars(key))
-
-    if key.keytype == "Axis" and key.number == 4:
-        # Right trigger button
-        # Throttle
-        print("Throttle")
-        print(vars(key))
-
-    if key.keytype == "Button" and key.number == 2:
-        # Red circle button
-        # Safe stop
-        print("Safe stop")
-        print(vars(key))
-
-    if key.keytype == "Button" and key.number == 3:
-        # Green triangle button
-        # Start
-        print("Start")
-        print(vars(key))
-
-    if key.keytype == "Hat" and key.number == 0 and key.raw_value == 1:
-        # Left hat up
-        # Increase speed limit
-        print("Increase speed limit")
-        print(vars(key))
-
-    if key.keytype == "Hat" and key.number == 0 and key.raw_value == 4:
-        # Left hat down
-        # Decrease speed limit
-        print("Decrease speed limit")
+    if key.keytype != "Axis":
         print(vars(key))
 
 run_event_loop(print_add, print_remove, key_received)
