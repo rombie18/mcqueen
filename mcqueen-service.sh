98e92ef1667c873540c6aeef2623648d2d2a867c
Wout Rombouts <woutrombouts18@gmail.com>
2023-03-18 22:43:28 +0100

    t

--- mcqueen-service.sh
+++ mcqueen-service.sh
@@ -5,7 +5,7 @@ nmcli monitor |
         if [[ "$line" == "wlan0: disconnected" ]]; then
             nmcli dev wifi hotspot ifname wlan0 ssid McQueen password _kachow_
         elif [[ "$line" == "wlan0: connected" ]]; then
-            python /home/mcqueen/main_program.py
+            python /home/mcqueen/mcqueen/main_program.py
         elif [[ "$line" == "wlan0: device removed" ]]; then
             pkill -f main_program.py
         fi
