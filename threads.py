438d831594c221c2be0e01d222dbefd8ea6f4737
Wout Rombouts <woutrombouts18@gmail.com>
2023-03-20 23:23:32 +0100

    d

--- threads.py
+++ threads.py
@@ -26,46 +26,15 @@ from datetime import datetime
 class Mcqueen:
     def __init__(self):
         # Variables
-        self.velocity = 0
         self.heading = 0
-        self.stop = False
-        self.pid_control = False
-        self.set_heading = 0
 
         # Busses
         print("Initialising busses...")
-        bus_i2c_1 = I2C(board.SCL, board.SDA)
         bus_i2c_2 = I2C(board.SCL_1, board.SDA_1)
-
-        # Other
-        print("Initialising PWM driver...")
-        pwmdriver = PCA9685(bus_i2c_1)
-        pwmdriver.frequency = 50
 
         # Sensors
         print("Initialising sensors...")
         self.sensor_imu = BNO055_I2C(bus_i2c_2)
-        # self.sensor_encoder = IncrementalEncoder(board.D10, board.D9)
-
-        # Actuators
-        print("Initialising actuators...")
-        # Steering servo rc car, T: 20.08ms, PW: (920µs to 2120µs) -> (1320µs to 1720µs) to limit dragging against axis
-        # angle 0 =~ 15° right, angle 180 =~ 15° left
-        self.actuator_servo = MOTOR.Servo(
-            pwmdriver.channels[0], min_pulse=1220, max_pulse=1820)
-        # Motor ESC rc car, T: 20.08ms, PW: (1000µs to 2000µs)
-        self.actuator_motor = MOTOR.ContinuousServo(
-            pwmdriver.channels[1], min_pulse=1000, max_pulse=2000)
-
-        # Controllers
-        print("Initialising controllers...")
-        self.servo_pid = PID(3.5, 6, 0.5, setpoint=self.transform_angle_to_centerangle(self.transform_heading_to_angle(self.set_heading)))
-        self.servo_pid.output_limits = (-90, 90)
-        self.servo_pid.sample_time = 0.1
-        # Max safe speed = 0.3,  Slow = 0.1,  AVG = 0.2
-        self.motor_pid = PID(1, 0, 0, setpoint=0.1)
-        self.motor_pid.output_limits = (0, 0.1)
-        self.motor_pid.sample_time = 0.1
 
         # Telemetry
         self.path = "data/" + datetime.now().strftime("%d%m%Y %H:%M:%S")
@@ -94,7 +63,7 @@ class Mcqueen:
                     
         pipe_sensor_imu = deque()
         thread_producer_sensor_imu = ProducerThread(pipe_sensor_imu, self.stop_event, 100, handle_produce=handle_produce_sensor_imu)
-        thread_reader_sensor_imu = ReaderThread(pipe_sensor_imu, self.stop_event, 100, handle_read=handle_read_sensor_imu)
+        thread_reader_sensor_imu = ReaderThread(pipe_sensor_imu, self.stop_event, 1, handle_read=handle_read_sensor_imu)
         thread_consumer_sensor_imu = ConsumerThread(pipe_sensor_imu, self.stop_event, 0.5, handle_consume=handle_consume_sensor_imu)
 
         thread_producer_sensor_imu.start()
