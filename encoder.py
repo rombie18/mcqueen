e2439b9425abe445a85df7bc17103b162fcd961c
Wout Rombouts <woutrombouts18@gmail.com>
2023-03-22 20:49:38 +0100

    s

--- encoder.py
+++ encoder.py
@@ -12,7 +12,7 @@ class Encoder:
         self.state = '00'
         self.direction = None
         self.callback = callback
-        print(GPIO.getmode())
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(self.leftPin, GPIO.BOTH, callback=self.transitionOccurred)  
