aff8dbafdd8081981546fb72e7df9b18500caf48
Wout Rombouts <woutrombouts18@gmail.com>
2023-04-21 13:12:35 +0200

    e

--- Software/threads/test.py
+++ Software/threads/test.py
@@ -2,39 +2,69 @@ import time
 import logging
 from collections import deque
 from threading import Event
-from customthreads import IMUThread, EncoderThread, StatsThread
+from customthreads import IMUThread, EncoderThread, StatsThread, ControllerThread, ImageProcessingThread
 
-logging.getLogger()
-logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(threadName)-16s | %(message)s")
-stop_event = Event()
+class McQueen:
+    def __init(self):
+        
+        logging.getLogger()
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(threadName)-16s | %(message)s")
 
-pipe_sensor_imu = deque(maxlen=100)
-pipe_sensor_encoder = deque(maxlen=100)
-pipe_sensor_stats = deque(maxlen=100)
+        logging.info("Initialising threads")
+        self.threads_init()
 
-threads = []
-threads.append(IMUThread(pipe_sensor_imu, stop_event))
-threads.append(EncoderThread(pipe_sensor_encoder, stop_event))
-threads.append(StatsThread(pipe_sensor_stats, stop_event))
+        logging.info("Starting all threads")
+        self.threads_start()
+        
+        logging.info("Starting main loop")
+        try:
+            while True:
+                
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            logging.fatal(e)
+        finally:
+            logging.info("Signaling threads to stop")
+            self.threads_stop()
 
-def threads_start():
-    for thread in threads:
-        thread.start()
 
-def threads_stop():
-    stop_event.set()
-    for thread in threads:
-        thread.join()
+    def threads_init(self):
+        self.stop_event = Event()
 
-threads_start()
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    logging.debug("KeyboardInterrupt: Stopping program")
-except Exception as e:
-    logging.fatal(e)
-finally:
-    logging.info("Signaling threads to stop")
+        self.pipe_sensor_imu = deque(maxlen=100)
+        self.pipe_sensor_encoder = deque(maxlen=100)
+        self.pipe_sensor_stats = deque(maxlen=100)
+        self.pipe_sensor_controller = deque(maxlen=100)
+        self.pipe_sensor_imageprocessing = deque(maxlen=100)
 
-    
+        self.threads = []
+        self.threads.append(IMUThread(self.pipe_sensor_imu, self.stop_event))
+        self.threads.append(EncoderThread(self.pipe_sensor_encoder, self.stop_event))
+        self.threads.append(StatsThread(self.pipe_sensor_stats, self.stop_event))
+        self.threads.append(ControllerThread(self.pipe_sensor_controller, self.stop_event))
+        self.threads.append(ImageProcessingThread(self.pipe_sensor_imageprocessing, self.stop_event))
+        
+    def threads_start(self):
+        for thread in self.threads:
+            thread.start()
+
+    def threads_stop(self):
+        self.stop_event.set()
+        for thread in self.threads:
+            thread.join()
+            
+            
+    def calculate_velocity(self):
+        self._current_encoder = self.pipe_sensor_encoder[-1]
+        if self._previous_encoder:
+            self.velocity = (self._current_encoder["position"] - self._previous_encoder["position"]) / (self._current_encoder["time"] - self._previous_encoder["time"])
+            
+        self._previous_encoder = self._current_encoder
+        
+    def calculate_heading(self):
+        self.heading = self.pipe_sensor_imu[-1]["euler"][0]
+
+            
+mcqueen = McQueen()
