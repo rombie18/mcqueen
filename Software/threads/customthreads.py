3bff231d0a2f3e50084ba64e75dc1ec3e8719753
Wout Rombouts <woutrombouts18@gmail.com>
2023-05-03 19:55:36 +0200

    e

--- Software/threads/customthreads.py
+++ Software/threads/customthreads.py
@@ -159,7 +159,7 @@ class ControllerThread(Thread):
             
             logging.info("Initialising Controller...")
             
-            mngr = pyjoystick.ThreadEventManager(event_loop=run_event_loop, add_joystick=self.__controller_add, remove_joystick=self.__controller_remove, handle_key_event=self.__controller_process, alive=self.__controller_alive)
+            mngr = pyjoystick.ThreadEventManager(event_loop=run_event_loop, add_joystick=self.__controller_add, remove_joystick=self.__controller_remove, handle_key_event=self.__controller_process)
             mngr.start()
             
             self.init_event.set()
@@ -195,9 +195,6 @@ class ControllerThread(Thread):
         }
         logging.debug("Controller event: " + str(key))
         self.pipe.append(data)
-        
-    def __controller_alive(self):
-        return self.stop_event.is_set()
         
 class ImageProcessingThread(Thread):
     def __init__(self, pipe, stop_event, init_event, pause_event):
