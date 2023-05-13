32c6434439042daff7c33c5d960aaf275607aca5
Wout Rombouts <woutrombouts18@gmail.com>
2023-05-13 20:03:01 +0200

    Finalising updates

--- Software/README.md
+++ Software/README.md
@@ -6,20 +6,24 @@
 
 A piece of python software to control an autonomous rc car with camera vision and odometry.
 
+## Working principle
+
+The program will start by initialising the actuators (motor + servo), PID controllers and the controller. Next all other sensors or functions will be initialised and run in parallel as threads. Once all threads are initialised, the program will release all threads to start processing or collecting/writing data. The data is send over a pipe that will interconnect the threads with the main program.
+
 ## Installation
 
 Please follow the steps below to install the software on your Jetson Nano.
 
-1. Clone this gif to your user directory:  
+1. Clone this repository to your user directory:  
    `cd ~`  
    `git clone https://github.com/rombie18/mcqueen`
 2. Run the installation script[^1] (with root permissions):  
-   `cd mcqueen`  
+   `cd mcqueen/Software/install`  
    `sudo -H bash ./install.sh`
 3. All done!
 
 ## Running the software
 
-As configured by the installation script, the software should automatically start running when the Jetson Nano boots up. If for any reason you would want the program to stop, you can gracefully terminate the process by executing `pkill -f main_program.py`. You can manually start the program by running the main python script `python main_program.py`.
+As configured by the installation script, the software should automatically start running when the Jetson Nano boots up. If for any reason you would want the program to stop, you can gracefully terminate the process by executing `sudo systemctl stop mcqueen`. You can manually start the program by running the main python script `python drive.py`.
 
-[^1]: _The installation script will configure some services automatically for you. This includes setting up VNC-access, automatically starting the main program on boot, starting a wifi hotspot on boot, automatically pulling the repository when internet is available,..._
+[^1]: _The installation script will install libraries and configure a services automatically for you. This includes updating libraries, installing circuitpython, installing and enabling service,..._
--- Software/main/drive.py
+++ Software/main/drive.py
@@ -1,69 +1,56 @@
-import sys
-sys.path.append("/home/mcqueen/mcqueen/Software/libs/encoder")
-sys.path.append("/home/mcqueen/mcqueen/Software/libs/tis")
-
-import board
 import time
-import json
-import cv2
-import numpy as np
+import logging
 import signal
+import traceback
+import os
+import board
+import Jetson.GPIO as GPIO
+import pyjoystick
 
-from simple_pid import PID
-from busio import I2C
-from adafruit_bno055 import BNO055_I2C
+from collections import deque
+from threading import Event
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo as MOTOR
-import pyjoystick
+from busio import I2C
+from simple_pid import PID
 from pyjoystick.sdl2 import run_event_loop
 
-from encoder import Encoder
-from tis import TIS, SinkFormats
-
+from customthreads import IMUThread, EncoderThread, StatsThread, DistanceProcessingThread, LowImageProcessingThread, SideImageProcessingThread, DataCollectionThread, ImageProcessingThread
 
 class McQueen:
-    # Main methods
     def __init__(self):
-        # Variables
+        # Init logger
+        logging.getLogger()
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s | %(levelname)s | %(threadName)s | %(message)s")
+        
+        # Init variables
+        self.state = "INITIALISING"
+        self.control = "AUTO"
+        
+        self.flag_initialised = False
+        
         self.velocity = 0
+        self.set_velocity = 0
         self.heading = 0
-        self.stop = False
-        self.start = False
-        self.pid_control = False
         self.set_heading = 0
         
-        self.current_position = 0
-        self.previous_position = 0
-        self.current_time = 0
-        self.previous_time = 0
+        self._current_encoder = None
+        self._previous_encoder = None
 
-        self.X_lb=0
-        self.Y_lb=0
-        self.X_rb=0
-        self.Y_rb=0
-        self.lower = np.array([150, 100, 20])
-        self.upper = np.array([180, 255, 255])
-        self.h1 = 0
-        self.h2 = 0
-
-        # Busses
-        print("Initialising busses...")
+        self.anglefactor = 1
+        
+        # Init threads
+        self.threads_init()
+        
+        # Init termination handlers
+        signal.signal(signal.SIGINT, self.safe_stop)
+        signal.signal(signal.SIGTERM, self.safe_stop)
+        
+        # Actuators
+        logging.info("Initialising actuators...")
         bus_i2c_1 = I2C(board.SCL, board.SDA)
-        bus_i2c_2 = I2C(board.SCL_1, board.SDA_1)
-
-        # Other
-        print("Initialising PWM driver...")
         pwmdriver = PCA9685(bus_i2c_1)
         pwmdriver.frequency = 50
-
-        # Sensors
-        print("Initialising sensors...")
-        self.sensor_imu = BNO055_I2C(bus_i2c_2)
-        self.sensor_encoder = Encoder(board.D6, board.D12)
-        # 162cm = 20 cycles op as encoder ==> per cycle = 8.1cm, 265 counts per cycle 
-
-        # Actuators
-        print("Initialising actuators...")
         # Steering servo rc car, T: 20.08ms, PW: (920µs to 2120µs) -> (1320µs to 1720µs) to limit dragging against axis
         # angle 0 =~ 15° right, angle 180 =~ 15° left
         self.actuator_servo = MOTOR.Servo(
@@ -73,87 +60,104 @@ class McQueen:
             pwmdriver.channels[1], min_pulse=1000, max_pulse=2000)
 
         # Controllers
-        print("Initialising controllers...")
+        logging.info("Initialising PID controllers...")
         # PID: throttle 0.3 -> 3.5, 6, 0.5
         self.servo_pid = PID(3.5, 0, 0.5, setpoint=self.transform_angle_to_centerangle(self.transform_heading_to_angle(self.set_heading)))
         self.servo_pid.output_limits = (-90, 90)
         self.servo_pid.sample_time = 0.1
         # Max safe speed = 0.3,  Slow = 0.1,  AVG = 0.2
-        self.motor_pid = PID(1, 0, 0, setpoint=0.1)
-        self.motor_pid.output_limits = (0, 0.2)
+        self.motor_pid = PID(1, 0, 0, setpoint=0.2)
+        self.motor_pid.output_limits = (0, 0)
         self.motor_pid.sample_time = 0.1
 
-        # Image processing
-        print("Initialising image processing...")
-        self.Tis = TIS()
-        self.Tis.open_device("02320237", 1280, 720, "60/1", SinkFormats.BGRA, False)
-
-        with open('/home/mcqueen/mcqueen/Software/main/camera_properties.json', 'r') as file:
-            data = json.load(file)
-            for key, item in data.items():
-                self.Tis.set_property(key, item)
-
-        try:
-            self.Tis.set_property("TriggerMode","Off")
-        except Exception as error:
-            print(error)
-
-        self.Tis.start_pipeline()
-        
-        # Init termination handlers
-        signal.signal(signal.SIGINT, self.safe_stop)
-        signal.signal(signal.SIGTERM, self.safe_stop)
-
-        try:
-            print("Starting main loop...")
-            self.main_loop()
-        except Exception as e:
-            print("-----------ERROR-----------")
-            print(e)
-            print("------------END------------")
-        finally:
-            print("Handling safe stop...")
-            self.safe_stop()
-
-    def main_loop(self):
-
+        logging.info("Starting controller")
         mngr = pyjoystick.ThreadEventManager(event_loop=run_event_loop, add_joystick=self.controller_add, remove_joystick=self.controller_remove, handle_key_event=self.controller_process)
         mngr.start()
 
-        while not self.stop:
-            self.calculate_velocity()
-            self.calculate_heading()
-            if self.pid_control:
-                self.cycle_loop_motor()
-                self.cycle_loop_steering()
-
-            if self.start:
-                self.actuator_servo.angle = self.do_image_proccess()
-
-
+        logging.info("Starting all threads")
+        self.threads_start()
+        
+        logging.info("Starting main loop")
+        try:
+            while not self.stop_event.is_set():
+                self.process_flags()
+                
+                #TODO improve state machine
+                if self.state == "INITIALISING":
+                    # Start all thread calulations when everything is initialised
+                    if self.flag_initialised:
+                        logging.info("Releasing all threads.")
+                        self.pause_event.clear()
+                        self.state = "RUNNING"
+                        
+                if self.state == "RUNNING":
+                    self.process_calculations()
+                    if self.control == "AUTO":
+                        self.process_control_loops()
+                    
+                time.sleep(0.1)
+                
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            logging.fatal(e)
+            traceback.print_exc()
+        finally:
+            self.safe_stop()
+            
+            
     def safe_stop(self):
-        self.stop = True
+        logging.info("Safe stop")
+        self.threads_stop()
         self.actuator_motor.throttle = 0
         self.actuator_servo.angle = self.transform_heading_to_angle(0)
-        self.Tis.stop_pipeline()
-        cv2.destroyAllWindows()
+        GPIO.cleanup()
 
-    # Control loops
+    ### Control loops ###
+    def process_control_loops(self):
+        self.__cycle_loop_motor()
+        self.__cycle_loop_steering()
 
-    def cycle_loop_motor(self):
-        self.actuator_motor.throttle = self.motor_pid(self.velocity)
+    def __cycle_loop_motor(self):
+        self.actuator_motor.throttle = 0#self.motor_pid(self.velocity)
 
-    def cycle_loop_steering(self):
-        self.actuator_servo.angle = self.transform_centerangle_to_angle(self.servo_pid(
-            self.transform_angle_to_centerangle(self.transform_heading_to_angle(self.heading))))
+    def __cycle_loop_steering(self):
+        if len(self.pipe_imageprocessing) > 0:
+            angle = self.pipe_imageprocessing[-1]["angle"] * self.anglefactor
+            if angle > 180:
+                angle = 180
+            self.actuator_servo.angle = self.pipe_imageprocessing[-1]["angle"] * self.anglefactor
+            #self.actuator_servo.angle = self.transform_centerangle_to_angle(self.servo_pid(self.transform_angle_to_centerangle(self.transform_heading_to_angle(self.heading))))
 
-    def calculate_velocity(self):
-        self.velocity = 0.1
+    ### Calculations ###
+    def process_calculations(self):
+        self.__calculate_velocity()
+        self.__calculate_heading()
+            
+    def __calculate_velocity(self):
+        # 162cm = 20 cycles op as encoder ==> per cycle = 8.1cm, 265 counts per cycle ==> per count = 0.3056603773584905660377358490566cm
+        if len(self.pipe_encoder) > 2:
+            self._current_encoder = self.pipe_encoder[-1]
+            if self._previous_encoder != None and self._current_encoder["time_epoch"] != self._previous_encoder["time_epoch"]:
+                self.velocity = -(self._current_encoder["position"] - self._previous_encoder["position"]) * 3.0566 / ((self._current_encoder["time_epoch"] - self._previous_encoder["time_epoch"]) * 1000)
+            self._previous_encoder = self._current_encoder
+        
+    def __calculate_heading(self):
+        if len(self.pipe_imu) != 0:
+            self.heading = self.pipe_imu[-1]["euler"][0]    
+            
+    ### Flags ###
+    def process_flags(self):
+        self.__flag_initialised()
 
-    def calculate_heading(self):
-        self.heading = self.sensor_imu.euler[0]
-
-    # Helper functions
+    def __flag_initialised(self):
+        for init_event in self.init_events:
+            if not init_event.is_set():
+                self.flag_initialised = False
+                return
+        self.flag_initialised = True
+        
+    ### Helper functions ###
     def transform_heading_to_angle(self, heading):
         if heading > 180:
             if heading > 270:
@@ -172,18 +176,18 @@ class McQueen:
     def transform_centerangle_to_angle(self, centerangle):
         return centerangle + 90
 
-    # Controller functions
+    ### Controller functions ###
     def controller_add(self, joy):
-        print('Controller connected:', joy)
+        logging.debug('Controller connected')
 
     def controller_remove(self, joy):
-        print('Controller disconnected:', joy)
+        logging.debug('Controller disconnected')
         # Robot sould stop here or at least continue in a very slow safe mode
-        self.stop = True
+        logging.warning('Controller disconnected, no controls available. Stopping for safety!')
+        self.stop_event.set()
 
     def controller_process(self, key):
         try:
-            print(key)
             if key.keytype == "Axis" and key.number == 0:
                 # Left joystick, left - right
                 # Steering
@@ -213,12 +217,15 @@ class McQueen:
             if key.keytype == "Button" and key.number == 2 and key.raw_value == 1:
                 # Red circle button
                 # Safe stop
-                self.stop = True
+                self.safe_stop()
 
             if key.keytype == "Button" and key.number == 3 and key.raw_value == 1:
                 # Green triangle button
                 # Start
-                self.start = not self.start
+                if self.pause_event.is_set(): 
+                    self.pause_event.clear()
+                else:
+                    self.pause_event.set()
 
             if key.keytype == "Hat" and key.number == 0 and key.raw_value == 1:
                 # Left hat up
@@ -250,53 +257,53 @@ class McQueen:
             print(e)
             print("-----------------CEND-----------------")
 
-    def do_image_proccess(self):
-        angle = 90
-        if self.Tis.snap_image(1):
-            video = self.Tis.get_image()
+    ### Threading ###
+    def threads_init(self):
+        self.stop_event = Event()
+        self.pause_event = Event()
+        self.pause_event.set()
+        
+        self.init_events = []
+        self.init_event_imu = Event()
+        self.init_event_encoder = Event()
+        self.init_event_stats = Event()
+        self.init_event_imageprocessing = Event()
+        self.init_event_datacollection = Event()
+        #self.init_events.append(self.init_event_imu)
+        self.init_events.append(self.init_event_encoder)
+        self.init_events.append(self.init_event_stats)
+        self.init_events.append(self.init_event_imageprocessing)
+        self.init_events.append(self.init_event_datacollection)
 
-            hight, width, _ = video.shape #Get resolution
+        self.pipe_imu = deque(maxlen=1000)
+        self.pipe_encoder = deque(maxlen=1000)
+        self.pipe_stats = deque(maxlen=1000)
+        self.pipe_controller = deque(maxlen=1000)
+        self.pipe_imageprocessing = deque(maxlen=1000)
 
-            video_hsv = cv2.cvtColor(video, cv2.COLOR_BGR2HSV) #Converting BGR image to HSV format
+        self.pipes = {
+            'imu': self.pipe_imu,
+            'encoder': self.pipe_encoder,
+            'stats': self.pipe_stats,
+            'controller': self.pipe_controller,
+            'imageprocessing': self.pipe_imageprocessing,
+        }
 
-            mask_video = cv2.inRange(video_hsv, self.lower, self.upper) # Masking the image to find our color
+        self.threads = []
+        #self.threads.append(IMUThread(self.pipe_imu, self.stop_event, self.init_event_imu, self.pause_event))
+        self.threads.append(EncoderThread(self.pipe_encoder, self.stop_event, self.init_event_encoder, self.pause_event))
+        self.threads.append(StatsThread(self.pipe_stats, self.stop_event, self.init_event_stats, self.pause_event))
+        self.threads.append(DistanceProcessingThread(self.pipe_imageprocessing, self.stop_event, self.init_event_imageprocessing, self.pause_event))
+        self.threads.append(DataCollectionThread(None, self.stop_event, self.init_event_datacollection, self.pause_event, self.pipes))
+        
+    def threads_start(self):
+        for thread in self.threads:
+            thread.start()
+
+    def threads_stop(self):
+        self.stop_event.set()
+        for thread in self.threads:
+            thread.join()
             
-            mask_leftBand = mask_video[0:hight, int(250):int(300)] #Crop right band
-            mask_rightBand = mask_video[0:hight, int(width-300):int(width-250)] #crop left band
-
-            mask_contours_leftBand, hierarchy = cv2.findContours(mask_leftBand, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #Finding contours in mask image (leftband)
-            mask_contours_rightBand, hierarchy = cv2.findContours(mask_rightBand, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #Finding contours in mask image (rightband)
-
-            # Finding position of all contours of the bands
-            if len(mask_contours_leftBand or mask_contours_rightBand) != 0:
-                for i_lb, k_rb in zip(mask_contours_leftBand, mask_contours_rightBand):
-                    if cv2.contourArea(i_lb) > 500:
-                        x1, y1, w1, self.h1 = cv2.boundingRect(i_lb)
-                        X_lb = int(x1+(w1/2)) #calculate center of rectangle
-                        Y_lb = int(y1+(self.h1/2))
-                        cv2.rectangle(video, (x1+250, y1), (x1 + w1+250, y1 + self.h1), (0, 0, 255), 3) #drawing rectangle
-                    
-                    if cv2.contourArea(k_rb) > 500:
-                        x2, y2, w2, self.h2 = cv2.boundingRect(k_rb)
-                        X_rb = int(width-(x2+(w2/2))) #calculate center of rectangle
-                        Y_rb = int(y2+(self.h2/2))
-                        cv2.rectangle(video, ((width-x2-300), y2), (((width-x2-300) + w2), (y2 + self.h2)), (255, 0, 0), 3) #drawing rectangle (formula needed, otherwise it will add it on the left side)
-
-                    
-                diff = 1
-                if self.h2 != 0:
-                    diff = self.h1/self.h2
-                if (diff > 0.9) and (diff < 1.1):
-                    print("Go straight")
-                if diff > 1.1: #h1>h2
-                    norm = (self.h1-self.h2)/self.h1
-                    angle = 90*norm
-                    print("Go right, with angle:", angle)
-                if diff < 0.9: #h1<h2
-                    norm = 1-((self.h2-self.h1)/self.h2)
-                    angle = 90+90*norm
-                    print("Go left, with angle:", angle)
-
-        return angle
-
+            
 mcqueen = McQueen()
